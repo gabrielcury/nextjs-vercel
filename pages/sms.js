6dfa15e38390951443ae6db07f17fe61ff61fd83
Gabriel <bielbanco@gmail.com>
2021-02-22 18:42:03 -0300

    Update sms.js

--- pages/sms.js
+++ pages/sms.js
@@ -8,7 +8,7 @@ function SMS(request, response) {
     alert(event.target.mensagem.value);
     
     const res = await fetch(
-      'https://webservices.twwwireless.com.br/reluzcap/wsreluzcap.asmx/enviasms/testecap/testecap',
+      'https://webservices.twwwireless.com.br/reluzcap/wsreluzcap.asmx/enviasms',
       {
         body: JSON.stringify({
           name: event.target.numero.value,
