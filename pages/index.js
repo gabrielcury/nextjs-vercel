7c66acaf50861b72626bd0823869436ae932dc76
Gabriel <bielbanco@gmail.com>
2021-02-22 16:06:46 -0300

    Create index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to SMS API<a href="https://nextjs.org">Built using Next.js!</a>
+          Welcome to SMS API Build using<a href="https://nextjs.org">Next.js!</a>
         </h1>
 
      
