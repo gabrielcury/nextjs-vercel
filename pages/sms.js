76217d8c8bb6dfa8192114224504a9bf4451e4b4
Gabriel <bielbanco@gmail.com>
2021-02-22 18:09:45 -0300

    Update sms.js

--- pages/sms.js
+++ pages/sms.js
@@ -7,17 +7,26 @@ function SMS(request, response) {
     alert(event.target.numero.value);
     alert(event.target.mensagem.value);
     
+    let idSMS  = "";
+    let senha  = "";
+    let numUsu = "";
+    
+    let numero = event.target.numero.value;
+    let mensagem = event.target.mensagem.value;
+    
     const res = await fetch(
-      'https://nextjs-two-rouge-20.vercel.app/sms',
+      `http://webservices2.twwwireless.com.br/reluzcap/wsreluzcap.asmx/EnviaSMS?NumUsu=${numUsu}&Senha=${senha}&SeuNum=${idSMS}&Celular=${numero}&Mensagem=${mensagem}`,
       {
+        /*
         body: JSON.stringify({
           name: event.target.numero.value,
           numero: event.target.mensagem.value
         }),
+        */
         headers: {
           'Content-Type': 'application/json'
         },
-        method: 'POST'
+        method: 'GET'
       }
     )
 
